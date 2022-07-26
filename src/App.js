dccc9e9206fe8df71c17908e800b37d18e9c8d99
ankitasweetsprinkles01 <goddeterminestherighttime@gmail.com>
2022-07-26 17:54:48 +0530

    AnkitaChatterjee|A91005219157|Amity

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Foodbox from './components/Foodbox';
+
+function App() {
+  return (
+    <div className="App">
+      <Foodbox/>
+    
+    </div>
+  );
+}
+
+export default App;
